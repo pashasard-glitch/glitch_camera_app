afacf8c208b222e486ee2f618c2c13c477ef3cc1
pashasard-glitch <pasha.sard@gmail.com>
2026-09-19 16:30:02 +0300

    Restore required profileLevel param with valid enum value for v1.7.2

--- lib/services/video_encoder_service.dart
+++ lib/services/video_encoder_service.dart
@@ -16,6 +16,7 @@ class VideoEncoderService {
       height: height,
       fps: fps,
       videoBitrate: 4 * 1000 * 1000,
+      profileLevel: VideoProfileLevel.avcProfileHigh_avcLevel4,
       audioBitrate: 0,
       audioChannels: 0,
       sampleRate: 0,
